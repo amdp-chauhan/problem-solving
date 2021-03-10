46593bcdd526d28d13193bc589e1f116f0db43bf
Amardeep Chauhan <amdp.chauhan@gmail.com>
2021-03-10 23:36:47 +0530

    Update print-words-in-reverse-order.py

--- print-words-in-reverse-order.py
+++ print-words-in-reverse-order.py
@@ -2,4 +2,4 @@ def print_reverse_words_in_sent(sent):
     print(sent)
     return [word[::-1] for word in sent.split(' ')]
 
-print_reverse_words_in_sent('This is my house')
+print(print_reverse_words_in_sent('This is my house'))
