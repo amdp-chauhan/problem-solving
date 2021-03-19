6b38428cf8768efef41487eeda030c31fd96e7a7
Amardeep Chauhan <amdp.chauhan@gmail.com>
2021-03-20 00:20:56 +0530

    Update largest-common-substring-in-a-given-list-of-strings.py

--- largest-common-substring-in-a-given-list-of-strings.py
+++ largest-common-substring-in-a-given-list-of-strings.py
@@ -1,24 +1,33 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        final_common = ''
-        if len(strs) == 0:
-            return ''
-        elif len(strs) == 1:
-            return strs[0]
-        initial = strs[0]
-        for item in strs[1:]:
-            index = 0
-            common = ''
-            final_common = ''
-            while len(initial)>index and len(item)>index:
-                #print(index, initial,len(initial), item,len(item))
-                if initial[index] == item[index]:
-                    common += initial[index]
-                    if len(common)>len(final_common):
-                        final_common = common
-                        #print(final_common)
-                else:
-                    common=''
-                index+=1
-            initial = final_common
-        return final_common       
+#### Longest common string
+def longestCommonSubstring(strs) -> str:
+
+    if len(strs) == 0:
+        return ''
+
+    result_substr = strs[0]
+
+    # iterate over substrings (string from second)
+    for item in strs[1:]:
+        index = 0
+        initial = result_substr
+        temp_common = ''
+        common = ''
+
+        # iterate over characters:
+        while len(initial)>index and len(item)>index:
+            # compare elements on a given index
+            if initial[index] == item[index]:
+                temp_common += initial[index]
+                # if new common is greater than previously obtained common
+                if len(temp_common)>len(common):
+                    common = temp_common
+            else:
+                # reset current common
+                temp_common=''
+            index+=1
+        # set new common as best common substr so far
+        result_substr = common
+        
+    return result_substr    
+
+print(longestCommonSubstring(['amaaarr', 'amaaaradee', 'amaaarmdee']))
