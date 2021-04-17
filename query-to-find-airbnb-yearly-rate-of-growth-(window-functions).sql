6962a5029450c26685c098186772eb5030377b04
Amardeep Chauhan <amdp.chauhan@gmail.com>
2021-04-17 23:21:11 +0530

    Update query-to-find-airbnb-yearly-rate-of-growth-(window-functions).sql

--- query-to-find-airbnb-yearly-rate-of-growth-(window-functions).sql
+++ query-to-find-airbnb-yearly-rate-of-growth-(window-functions).sql
@@ -2,6 +2,9 @@
 Question:
 
 Growth of Airbnb
+-----------------
+Ref: https://platform.stratascratch.com/coding-question?id=9637&python=
+
 Estimate the growth of Airbnb each year using the number of hosts registered as the growth metric. The rate of growth is calculated by taking ((number of hosts registered in the current year - number of hosts registered in the previous year) / the number of hosts registered in the previous year) * 100.
 Output the year, number of hosts in the current year, number of hosts in the previous year, and the rate of growth. Round the rate of growth to the nearest percent and order the result in the ascending order based on the year.
 Assume that the dataset consists only of unique hosts, meaning there are no duplicate hosts listed.
