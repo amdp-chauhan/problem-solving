9e69f541836143864d46e3fb13998f96ebfbd42d
Amardeep Chauhan <amdp.chauhan@gmail.com>
2021-05-06 20:18:07 +0530

    Update find average_searches for booking vs non-booking.sql

--- sql/find average_searches for booking vs non-booking.sql
+++ sql/find average_searches for booking vs non-booking.sql
@@ -3,7 +3,9 @@ Bookings vs Non-Bookings
 ------------------------
 Reference: https://platform.stratascratch.com/coding-question?id=10124&python=
 
-Display the number of times a user performed a search which led to a successful booking and the number of times a user performed a search but did not lead to a booking. The output should have a column named action with values 'does not book' and 'books' as well as a 2nd column named average_searches with the average number of searches per action. Consider that the booking did not happen if the booking date is null.
+Display the number of times a user performed a search which led to a successful booking and the number of times a user performed a search but did not lead to a booking. 
+The output should have a column named action with values 'does not book' and 'books' as well as a 2nd column named average_searches with the average number of searches per action. 
+Consider that the booking did not happen if the booking date is null.
 
 Table: airbnb_contacts, airbnb_searches
 
