431a89227c8c63769b7a66f9e5b33a22c57e41a6
Amardeep Chauhan <amdp.chauhan@gmail.com>
2022-08-24 21:26:36 +0530

    Create 180 - Consecutive Numbers.sql

---
+++ sql/180 - Consecutive Numbers.sql
@@ -0,0 +1,57 @@
+/*
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
+
+
+-- SOLUTION 
+
+/* Write your T-SQL query statement below */
+SELECT DISTINCT out.num as ConsecutiveNums 
+FROM (SELECT id, num, 
+LAG(num, 1) OVER (order BY id) as lag1, 
+LAG(num, 2) OVER (order BY id) as lag2
+FROM Logs) out
+WHERE 
+out.num = out.lag1 AND
+out.lag1 = out.lag2
