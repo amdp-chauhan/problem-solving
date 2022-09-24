c16015fec37edd6f46a1f22fb280e583a22edc2a
Amardeep Chauhan <amdp.chauhan@gmail.com>
2022-09-24 20:36:07 +0530

    Create 4 Sum.py

---
+++ python/4 Sum.py
@@ -0,0 +1,38 @@
+
+"""
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+"""
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        sorted_arr = sorted(nums)
+        result = []
+        for i in range(len(sorted_arr)):
+            for j in range(len(sorted_arr)-1, i, -1):
+                left = i+1
+                right = j-1
+                while left<right:
+                    items4 = [sorted_arr[i], sorted_arr[left], sorted_arr[right], sorted_arr[j]]
+                    item4_sum = sum(items4)
+                    # print(i, left, right, j, f'sum:{item4_sum}')
+                    if item4_sum==target:
+                        if not items4 in result: result.append(items4)
+
+                    if item4_sum>target:
+                        right -= 1
+                    else:
+                        left += 1
+    
+        # print("result - ",result)
+        return result
+                    
